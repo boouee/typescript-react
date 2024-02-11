d7fe231360efaf1afd82095ac7e8091b48d54f93
boouee <104202645+boouee@users.noreply.github.com>
2024-02-11 19:24:44 +0300

    Update options.tsx

--- src/components/options.tsx
+++ src/components/options.tsx
@@ -1,77 +1,112 @@
-//import Select, { SelectChangeEvent } from "@mui/material/Select";
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
-import MuiInput from "@mui/material/Input";
-//import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import { styled } from "@mui/material/styles";
 
-const Input = styled(MuiInput)`
-  width: 42px;
-`;
-export function ItemSelect({ func, label, value }: any) {
-  const options = ["10", "20", "30", "40", "50"];
+export function ItemSelect({ func, props, disabled }: any) {
   return (
     <select
       style={{
-        position: "absolute",
-        margin: "auto",
-        top: "25px",
-        left: "10px",
+        //position: "absolute",
+        //margin: "auto",
+        width: "100%",
       }}
+      onChange={func}
+      disabled={disabled}
     >
-      <option value="fruit">Fruit</option>
-      <option value="vegetable">Vegetable</option>
-      <option value="meat">Meat</option>
+      <option value={""}></option>
+      {props.options.map((option: any) => (
+        <option key={option.v} value={option.v}>
+          {option.l}
+        </option>
+      ))}
     </select>
   );
 }
-export function ItemRadio({ func, label, value }: any) {
+export function ItemRadio({ func, props }: any, disabled: boolean) {
   return (
-    <RadioGroup
-      row
-      aria-labelledby="demo-radio-buttons-group-label"
-      defaultValue="female"
-      name="radio-buttons-group"
-      onChange={func}
-    >
-      <FormControlLabel value="female" control={<Radio />} label="on" />
-      <FormControlLabel value="male" control={<Radio />} label="off" />
-    </RadioGroup>
+    <div onChange={func}>
+      <input
+        type="radio"
+        id="html"
+        name="fav_language"
+        value="HTML"
+        disabled={disabled}
+      />
+      <br />
+      <input
+        type="radio"
+        id="html"
+        name="fav_language"
+        value="HTML"
+        disabled={disabled}
+      />
+    </div>
   );
 }
-export function ItemInput({ func, label, value }: any) {
+export function ItemInput({ func, props, disabled }: any) {
   return (
-    <Input
-      value={value}
-      size="small"
-      inputProps={{
-        step: 10,
-        min: 0,
-        max: 100,
-        type: "number",
-        "aria-labelledby": "input-slider",
-      }}
+    <input
+      type="number"
+      id="quantity"
+      name="quantity"
+      min={props.min}
+      max={props.max}
+      step={props.step}
       onChange={func}
+      disabled={disabled}
     />
   );
 }
-function OptionType({ func, label, value, type }: any) {
-  if (type == 0) {
-    return <ItemSelect func={func} label={label} value={value} />;
-  } else if (type == 1) {
-    return <ItemInput func={func} label={label} value={value} />;
+function OptionType({ func, props, disabled }: any) {
+  if (props.type == "dropdown") {
+    return <ItemSelect func={func} props={props} disabled={disabled} />;
+  } else if (props.type == "int") {
+    return <ItemInput func={func} props={props} disabled={disabled} />;
+  } else if (props.type == "checkbox") {
+    return <ItemRadio func={func} props={props} disabled={disabled} />;
   } else {
-    return <ItemRadio func={func} label={label} value={value} />;
+    return <></>;
   }
 }
-export default function OptionItem({ func, label, value, type }: any) {
+export default function OptionItem({ func, props, disabled }: any) {
   return (
     <FormControl
       sx={{ m: 1, minWidth: 100 }}
       size="small"
-      style={{ position: "absolute", top: "10px", left: "10px" }}
-    ></FormControl>
+      style={{
+        position: "absolute",
+        top: "0px",
+        width: "100px",
+        height: "100%",
+        textAlign: "center",
+        //display: "inline-block",
+      }}
+    >
+      <div
+        style={{
+          userSelect: "none",
+          position: "absolute",
+          top: "5px",
+          fontSize: "15px",
+        }}
+      >
+        {props.label}
+        <br />
+        <br />
+      </div>
+      <div style={{ userSelect: "none", position: "absolute", bottom: "25px" }}>
+        <OptionType
+          func={func}
+          props={props}
+          disabled={disabled}
+          style={{
+            position: "relative",
+            border: "1px solid black",
+            bottom: "0px",
+            userSelect: "none",
+          }}
+        />
+      </div>
+    </FormControl>
   );
 }
